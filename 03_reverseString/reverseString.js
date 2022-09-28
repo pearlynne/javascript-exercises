a22d04f0ee71ed80d308d39a9333b82fd4548965
pearlynne <pearlynnecalleigh@gmail.com>
2022-09-27 17:35:53 -0700

    Fix reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
-};
+const reverseString = function (word) {
+    let output = word.split('');
+    let newstring = "";
+    //let arr = [];
+    for (i = output.length - 1; i >= 0; i--) {
+     // arr.push(output[i]);
+      newstring += output[i];
+    }
+  
+    //return arr;
+    return newstring;
+  
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
