bb848ef8ff86a517f96caa2c14a1bcff8908f794
pearlynne <pearlynnecalleigh@gmail.com>
2022-09-27 17:36:19 -0700

    Fix sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    let sum = start;
+    do {
+        start ++;
+        sum = start + sum;
+        //console.log(start, sum)
+    } while (end > start);
+return sum
 };
 
 // Do not edit below this line
