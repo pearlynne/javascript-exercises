6d79771537c7cd19960514ce7d8dab123b43c012
pearlynne <pearlynnecalleigh@gmail.com>
2022-09-27 17:35:41 -0700

    Fix repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    //let output = str.repeat(num)
+    //return output;
+    let output = "";
+        for (let i = 1; i <= num; i++) {
+        output += str
+    }
+    return output
 };
 
 // Do not edit below this line
