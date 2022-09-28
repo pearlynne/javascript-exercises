c781d6be0d02f3c626459d679b1bc1af5373932b
pearlynne <pearlynnecalleigh@gmail.com>
2022-09-27 17:36:08 -0700

    Fix removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...item) {
+    for (i = 0; i < arr.length; i++) {
+        //console.log('i is '+ [i] + ' value is ' + arr[i])
+        for (j = 0; j < item.length; j++) {
+        //    console.log('j is ' + [j] + ' value is '  + item[j])
+            if (arr[i] != item[j]) { 
+        //        console.log(arr[i] + ' is the same as ' + item[j])
+                continue }
+                arr.splice(i,1)
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
